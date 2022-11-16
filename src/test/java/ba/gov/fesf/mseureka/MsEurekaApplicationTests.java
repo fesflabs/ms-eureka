02197b815635120415620491e5a38876433df8a1
Gilberto Cosme <50690543+gilbertocosme@users.noreply.github.com>
2022-11-16 11:13:56 -0300

    Criação do projeto Spring

---
+++ src/test/java/ba/gov/fesf/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.gov.fesf.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
